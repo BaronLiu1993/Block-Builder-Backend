650e298362cd0861e7b0bf9bb309f6ed5d653c01
BaronLiu <baronliu1993@gmail.com>
2025-07-19 01:05:57 -0400

    init

---
+++ services/create.js
@@ -0,0 +1,38 @@
+
+//Chunk into Sections i
+/**
+    abstractedBlocks: [
+        titleBlock: {
+            coverTitle: 
+            coverSubtitle:
+            coverImage: //Link do not make it a blob at all
+        },
+        paragraphBlock: {
+            paragraphText: 
+            styling: []
+        }
+    ]   
+ */
+
+//Creates a new Database Entry and Then Creates an Abstracted Vercel Function
+//That pushes to vercel adds to code base and then adds it properly
+
+//Generate a HTML Tree That Fix
+//Select Custom Fonts and Add
+
+export async function parseTree(abstractedBlocks) {
+    let treeMap = {}
+    for (const i = 0; i < abstractedBlocks.length; i++) {
+        if (abstractedBlocks[i] == "titleBlock") {
+            
+        } else if (abstractedBlocks[i] == "paragraphBlock") {
+
+        } else {
+            
+        }
+    }
+}
+
+export async function generateHTMLTree() {
+    
+}
